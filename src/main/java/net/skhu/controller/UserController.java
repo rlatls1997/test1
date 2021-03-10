8d9335b09f54d6f4007d7815d955087abb467ea7
sinhyuk <rlatls1997@gmail.com>
2021-03-11 01:26:56 +0900

    test

--- src/main/java/net/skhu/controller/UserController.java
+++ src/main/java/net/skhu/controller/UserController.java
@@ -1,6 +1,6 @@
 package net.skhu.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller; //test
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
